d6e5028979ff35bd73e065f53f9876a4bb77ff58
Clowiser <jessica.rigaud@le-campus-numerique.fr>
2021-09-21 09:58:17 +0200

    1 - Mise en place des URL dans votre fichier de route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () { -> route avec la méthode get car on travaille avec l'URL + / de l'url et la fonction qui est appelée et exécutée
+//     return view('welcome'); -> il retourne la vue(view) 'welcome.blade.php avec les informations HTML et autres qui sont dedans
+// });
+
+//  1 - Mise en place des URL dans votre fichier de route
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home page'; // -> dans cette méthode on retourne pas par la view mais directement en notant ce que l'on veut retourner
+});
+
+Route::get('/product', function () {
+    return 'Liste des produits';
+});
+
+Route::get('/product/{id}', function ($id) { // Paramètre dynamique -> la fonction magique - a revoir
+    return 'Produit' .$id;
+});
+
+Route::get('/cat', function () {
+    return 'Panier';
 });
